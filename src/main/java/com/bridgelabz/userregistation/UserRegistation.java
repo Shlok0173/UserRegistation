aa95c72e7a4326c27e540024ac3250695f5745c0
Shlok Kumar Mishra <shlok7869@gmail.com>
2023-04-13 19:52:53 +0530

    Throw Custom Exception

--- src/main/java/com/bridgelabz/userregistation/UserRegistation.java
+++ src/main/java/com/bridgelabz/userregistation/UserRegistation.java
@@ -3,15 +3,77 @@ package com.bridgelabz.userregistation;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class UserRegistation {
+public class UserRegistation  {
 
-	 public boolean firstName(String testString){
-	        String regex = "[A-Z]{1}[a-z]*";
-	         Pattern pattern = Pattern.compile(regex);
-	        Matcher matcher = pattern.matcher(testString);
-	        boolean valid=matcher.find();
-	        return valid;
-	      
-	        }
+	// FirstName Method
+	public boolean firstName(String firstName) throws UserRegistationException{
+		String regex = "^[A-Z]{1}[a-zA-Z][a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(firstName);
+		
+		if (matcher.matches()) {
+			return true;
+		} else {
+			throw new UserRegistationException(UserRegistationException.ExceptionType.INVALID_FIRST_NAME,"Invalid first name");
+		}
+	
+		
+	}
+
+	// LastName Method
+
+	   public boolean lastName(String lastName)throws UserRegistationException{
+		   String regex="^[A-Z]{1}[a-zA-Z][a-z]{2,}$";
+		   Pattern pattern=Pattern.compile(regex);
+		   Matcher matcher=pattern.matcher(lastName);
+		   if(matcher.matches()) {
+			   return true;
+		   }else {
+			   throw new UserRegistationException(UserRegistationException.ExceptionType.INVALID_LAST_NAME,"Invalid Last Name");
+		   }
+	   }
+	
+	   //Email Method
+	public boolean email(String email) throws UserRegistationException {
+		String regex="^[a-z][a-z0-9]*@[a-z]*.[a-z]*$";
+		Pattern pattern=Pattern.compile(regex);
+		Matcher matcher=pattern.matcher(email);
+		boolean valid=matcher.matches();
+		if(matcher.matches()) {
+			return true;
+		}else {
+			throw new UserRegistationException(UserRegistationException.ExceptionType.INVALID_EMAIL,"Invalid Email");
+		}
+		
+
+	}
+	
+	//MobileNumber Method
+	public boolean mobileNumber(String Mnumber) throws UserRegistationException {
+		String regex="^([+][0-9])+[0-9]*$";
+		Pattern pattern=Pattern.compile(regex);
+		Matcher matcher=pattern.matcher(Mnumber);
+		boolean valid=matcher.matches();
+		if(valid==true) {
+			return valid;
+		}else {
+			throw new UserRegistationException(UserRegistationException.ExceptionType.INVALID_PHONE_NUMBER,"Invalid Mnumber");
+		}
+		
+	}
+	
+	//Password Method
+	public boolean password(String Password) throws UserRegistationException{
+		String regex="^[A-Z]{1}[a-z]*@[0-9]*$";
+		Pattern pattern=Pattern.compile(regex);
+		Matcher matcher=pattern.matcher(Password);
+		boolean valid=matcher.matches();
+		if(valid==true) {
+			return true;
+		}else {
+			throw new UserRegistationException(UserRegistationException.ExceptionType.INVALID_PASSWORD,"Invalid Password");
+		}
+	}
+		
+	
 }
-
--- src/test/java/com/bridgelabz/userregistation/Junit.java
+++ src/test/java/com/bridgelabz/userregistation/Junit.java
@@ -1,14 +1,119 @@
 package com.bridgelabz.userregistation;
 
+import static org.junit.Assert.assertTrue;
+
+import javax.xml.validation.Validator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Junit {
-      UserRegistation user=new UserRegistation();
-      
-      
-      @Test
-      
-      public void MatchFirstName() {
-    	  boolean result1=user.firstName("Shlok");
-    	  assertEqual(result1,true);
-      }
-      
+
+	@Test
+
+	public void matchFirstName() {
+		UserRegistation user = new UserRegistation();
+		try {
+			boolean result1 = user.firstName("Shlok");
+			Assert.assertTrue(result1);
+		} catch (UserRegistationException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void InvalidFirstName() {
+		UserRegistation user = new UserRegistation();
+		try {
+			boolean result1 = user.firstName("98989");
+		} catch (UserRegistationException e) {
+			Assert.assertEquals(UserRegistationException.ExceptionType.INVALID_FIRST_NAME, e.type);
+		}
+	}
+
+	@Test
+	public void matcherLastName() {
+		UserRegistation user = new UserRegistation();
+		try {
+			boolean result1 = user.lastName("Mishra");
+			Assert.assertEquals(true, result1);
+		} catch (UserRegistationException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void invalidLastName() {
+		UserRegistation user = new UserRegistation();
+		try {
+			boolean result1 = user.lastName("mishra");
+		} catch (UserRegistationException e) {
+			Assert.assertEquals(UserRegistationException.ExceptionType.INVALID_LAST_NAME, e.type);
+		}
+	}
+
+	@Test
+	public void matcherEmail() {
+		UserRegistation user = new UserRegistation();
+		try {
+			boolean result = user.email("shlok01@gmil.com");
+			Assert.assertEquals(true, result);
+		} catch (UserRegistationException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+
+	public void invalidEmail() {
+		UserRegistation user = new UserRegistation();
+		try {
+			boolean result = user.email("SHLOk@gmail.com");
+		} catch (UserRegistationException e) {
+			Assert.assertEquals(UserRegistationException.ExceptionType.INVALID_EMAIL, e.type);
+
+		}
+	}
+
+	@Test
+	public void matcherMobileNumber() {
+		UserRegistation user = new UserRegistation();
+		try {
+			boolean result = user.mobileNumber("+919630311021");
+			Assert.assertEquals(true, result);
+		} catch (UserRegistationException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Test
+	public void invalidMobileNumber() {
+		UserRegistation user = new UserRegistation();
+		try {
+			boolean result = user.mobileNumber("92293030033");
+		} catch (UserRegistationException e) {
+			Assert.assertEquals(UserRegistationException.ExceptionType.INVALID_PHONE_NUMBER, e.type);
+		}
+	}
+
+	@Test
+	public void matcherPassword() {
+		UserRegistation user=new UserRegistation();
+		try {
+		boolean result=user.password("Shlok@123");
+		Assert.assertEquals(true, result);
+	}catch(UserRegistationException e) {
+		e.printStackTrace();
+	}
+}
+	@Test
+	public void InvalidPassword() {
+		UserRegistation user=new UserRegistation();
+		try {
+			boolean result=user.password("SHLOKWPP1233");
+		}catch(UserRegistationException e) {
+			Assert.assertEquals(UserRegistationException.ExceptionType.INVALID_PASSWORD,e.type);
+		}
+	}
 }
